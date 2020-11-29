ba42cc358a43469c8ed1b96e65c035344e9931c9
Daave <daave@widgetbot.io>
2020-11-29 23:25:13 +0000

    Flyway Migrations

--- src/main/scala/venix/hookla/App.scala
+++ src/main/scala/venix/hookla/App.scala
@@ -1,7 +1,26 @@
 package venix.hookla
 
+import com.google.inject.Guice
 import com.twitter.server.TwitterServer
+import org.flywaydb.core.Flyway
 
 object App extends TwitterServer {
+  import net.codingwell.scalaguice.InjectorExtensions._
 
+  protected val injector = Guice.createInjector(new MainModule)
+  private val config = injector.instance[HooklaConfig]
+
+  private def migrateDb(): Unit = {
+    val flyway = new Flyway()
+    flyway.setDataSource(
+      config.flywayConfig.url,
+      config.flywayConfig.user,
+      config.flywayConfig.password
+    )
+    flyway.migrate()
+  }
+
+  def main(): Unit = {
+    migrateDb()
+  }
 }
--- src/main/scala/venix/hookla/HooklaConfig.scala
+++ src/main/scala/venix/hookla/HooklaConfig.scala
@@ -1,3 +1,23 @@
 package venix.hookla
 
-case class HooklaConfig ()
+case class HooklaConfig (
+  environment: String,
+  app: AppConfig,
+  postgres: PostgresConfig,
+  flywayConfig: FlywayConfig,
+)
+
+case class AppConfig(
+  port: Int
+)
+
+case class PostgresConfig(
+  url: String,
+  connectionTimeout: Int
+)
+
+case class FlywayConfig(
+  url: String,
+  user: String,
+  password: String
+)
