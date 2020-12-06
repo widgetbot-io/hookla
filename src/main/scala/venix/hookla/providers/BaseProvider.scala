f0aa9fae2a6caf2dbe859c1d17d787468355284f
viction <viction.dev@gmail.com>
2020-12-06 21:57:37 +0000

    feat(providers): Implement ability to specify header or body key for token/event type

--- src/main/scala/venix/hookla/providers/BaseProvider.scala
+++ src/main/scala/venix/hookla/providers/BaseProvider.scala
@@ -1,10 +1,15 @@
 package venix.hookla.providers
 
 trait BaseProvider {
+  def id: String
   def name: String
   def logo: String
-  def eventHeader: Option[String]
-  def tokenHeader: Option[String]
+
+  def eventHeader: Option[String] = None
+  def tokenHeader: Option[String] = None
+  def eventBodyKey: Option[String] = None
+  def tokenBodyKey: Option[String] = None
+
 
   def events: List[Nothing] // What type innit
 }
--- src/main/scala/venix/hookla/providers/gitlab/GitlabProvider.scala
+++ src/main/scala/venix/hookla/providers/gitlab/GitlabProvider.scala
@@ -2,9 +2,13 @@ package venix.hookla.providers.gitlab
 
 import venix.hookla.providers.BaseProvider
 
-class Provider extends BaseProvider {
+class GitlabProvider extends BaseProvider {
+  def id = "gitlab"
   def name = "Gitlab"
   def logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/GitLab_Logo.svg/1108px-GitLab_Logo.svg.png"
 
-  def events = List()
+  override def eventHeader = Some("X-Gitlab-Event")
+  override def tokenHeader = Some("X-Gitlab-Token")
+
+  def events = Nil
 }
