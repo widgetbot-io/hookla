85ee19cf7ded891ce2b6a92b0b98b4217e95e168
Daave <daave@widgetbot.io>
2020-12-06 23:50:42 +0000

    feat: Implement working webhooks

--- src/main/scala/venix/hookla/controllers/WebhookController.scala
+++ src/main/scala/venix/hookla/controllers/WebhookController.scala
@@ -7,42 +7,44 @@ import com.twitter.finagle._
 import com.twitter.finagle.http.Response
 import com.twitter.io.Buf
 import com.twitter.io.Buf.ByteArray
-import io.circe.Json
+import io.circe.{Codec, Encoder, Json, derivation}
 import io.circe.syntax._
-import io.circe.generic.auto._
 import io.finch.circe._
 import io.finch._
 import java.time.OffsetDateTime
 import javax.inject.Inject
 import venix.hookla.services.ProviderService
+import io.circe.generic.semiauto._
 
 case class OutgoingWebhookPayload(
     embeds: List[OutgoingEmbed]
 )
 
+object OutgoingWebhookPayload {
+  implicit val outgoingWebhookPayloadEncoder: Encoder[OutgoingWebhookPayload] = deriveEncoder
+}
+
 class WebhookController @Inject()(
   providerService: ProviderService
 ) extends BaseController {
+  import ackcord.data.DiscordProtocol._ // Codecs for Discord Objects.
+
   private val discordClient: Service[http.Request, http.Response] =
     Http.client.withSessionQualifier.noFailFast.withSessionQualifier.noFailureAccrual
-      .withTls("0b6e37a53a23.ngrok.io")
-      .newService("0b6e37a53a23.ngrok.io" + ":443")
-
+      .withTls("discordapp.com")
+      .newService("discordapp.com:443")
 
   def endpoints: Endpoint[IO, String] = process
 
   def pathProviderId: Endpoint[IO, String] = path[String]
-
   def process: Endpoint[IO, String] = post("process" :: pathProviderId :: jsonBody[Json] :: headersAll) { (providerId: String, body: Json, headers: Map[String, String]) =>
     val provider = providerService.getById(providerId)
 
-    println("hit")
-
     provider match {
       case None => Ok("success")
-      case Some(provider) => {
-
+      case Some(provider) =>
         val embed = OutgoingEmbed(
+          description = Some("Description type beat"),
           author = Some(OutgoingEmbedAuthor("viction", None, Some("https://i.viction.dev/assets/images/avi.png"))),
           url = Some("https://github.com/widgetbot-io/hookla"),
           timestamp = Some(OffsetDateTime.now()),
@@ -51,17 +53,15 @@ class WebhookController @Inject()(
         )
 
         val payload = OutgoingWebhookPayload(embed :: Nil)
-
-        val request = http.Request(http.Method.Post, "memes")
+        val request = http.Request(http.Method.Post, "/api/webhooks/ID/SECRET")
 
         request.write(payload.asJson.toString())
+        request.headerMap.add("Content-Type", "application/json")
 
-        println(payload.asJson.toString())
 
         discordClient(request) onSuccess { res: Response =>
         res.reader.read() map { meme =>
           val Buf.Utf8(str) = meme.get
-
           println(str)
         }
 
@@ -70,8 +70,7 @@ class WebhookController @Inject()(
           println(ex)
         }
 
-        Ok("success but actually a success")
-      }
+        Ok("success")
     }
   }
 }
