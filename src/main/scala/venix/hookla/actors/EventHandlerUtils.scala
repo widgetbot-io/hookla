61650c34bb0438b212700f474b1d531cfce8f790
Daave <daave@widgetbot.io>
2020-12-09 22:15:03 +0000

    feat: Implement private commits, including revert support!

--- src/main/scala/venix/hookla/actors/EventHandlerUtils.scala
+++ src/main/scala/venix/hookla/actors/EventHandlerUtils.scala
@@ -1,6 +1,10 @@
 package venix.hookla.actors
 
 trait EventHandlerUtils {
-  protected def formatCommit(message: String, length: Int): String =
-    s"${if (length > 1) "- " else ""}${message.replace("\n", "")}"
+  protected def formatCommit(message: String, length: Int): String = {
+    val privateDenotations = List("!", "$", "Revert !", "Revert $")
+    val isPrivate = privateDenotations.exists(message.startsWith)
+
+    s"${if (length > 1) "- " else ""}${if (isPrivate) "This commit message has been marked as private." else message.replace("\n", "")}"
+  }
 }
