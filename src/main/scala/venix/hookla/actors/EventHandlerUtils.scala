a897891ce8be5b8c94d57b66ba0b787ffcca6396
Daave <daave@widgetbot.io>
2020-12-17 10:05:34 +0000

    fix: Descriptions getting put on the same line

--- src/main/scala/venix/hookla/actors/EventHandlerUtils.scala
+++ src/main/scala/venix/hookla/actors/EventHandlerUtils.scala
@@ -5,6 +5,6 @@ trait EventHandlerUtils {
     val privateDenotations = List("!", "$", "Revert !", "Revert $")
     val isPrivate = privateDenotations.exists(message.startsWith)
 
-    s"${if (length > 1) "- " else ""}${if (isPrivate) "This commit message has been marked as private." else message.replace("\n", "")}"
+    s"${if (length > 1) "- " else ""}${if (isPrivate) "This commit message has been marked as private." else message}"
   }
 }
