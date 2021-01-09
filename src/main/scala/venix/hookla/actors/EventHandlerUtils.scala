befd8da82121de56647341e0d9d07559b218fa13
Daave <daave@widgetbot.io>
2021-01-09 22:51:31 +0000

    feat: Multiple private chars

--- src/main/scala/venix/hookla/actors/EventHandlerUtils.scala
+++ src/main/scala/venix/hookla/actors/EventHandlerUtils.scala
@@ -4,17 +4,19 @@ import venix.hookla.models.EmbedOptions
 
 trait EventHandlerUtils {
   protected def formatCommit(message: String, length: Int, embedOptions: Option[EmbedOptions]): String = {
-    val defaultChar = "!"
+    val defaultChar = "!" :: "$" :: Nil
+    val defaultReverts = defaultChar.map(c => s"Revert ${c}")
     val defaultMsg = "This commit message has been marked as private."
 
     embedOptions.fold {
-      val privateDenotations = defaultChar :: s"Revert $defaultChar" :: Nil
+      val privateDenotations = defaultChar ::: defaultReverts ::: Nil
       val isPrivate = privateDenotations.exists(message.startsWith)
 
       s"${if (length > 1) "- " else ""}${if (isPrivate) defaultMsg else message}"
     } { embedOptions =>
-      val privateChar = embedOptions.privateCharacter.getOrElse(defaultChar)
-      val privateDenotations = privateChar :: s"Revert $privateChar" :: defaultChar :: s"Revert $defaultChar" :: Nil
+      val privateChar: List[String] = embedOptions.privateCharacter.map(List(_)).getOrElse(defaultChar)
+      val privateReverts = privateChar.map(c => s"Revert ${c}")
+      val privateDenotations = privateChar ::: privateReverts ::: defaultChar ::: defaultReverts ::: Nil
       val isPrivate = privateDenotations.exists(message.startsWith)
 
       s"${if (length > 1) "- " else ""}${if (isPrivate) embedOptions.privateMessage.getOrElse(defaultMsg) else message}"
