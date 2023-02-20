7a7af586fcf9f652196ff4d6f69e3f26690d76a3
Daave <lewis@widgetbot.io>
2023-02-20 21:54:23 +0000

    refactor: Make private

--- src/main/scala/venix/hookla/util/Colours.scala
+++ src/main/scala/venix/hookla/util/Colours.scala
@@ -1,16 +1,16 @@
 package venix.hookla.util
 
 object Colours {
-  type Colour = Int
+  private type Colour = Int
 
-  val PUSH: Colour = 0x0da2ff
-  val NOTE: Colour = 0xffa500
+  val PUSH: Colour = 0x0DA2FF
+  val NOTE: Colour = 0xFFA500
 
   val CREATED: Colour = 0x08000
   val DELETED: Colour = 0xFF0000
 
-  val FAILED: Colour = 0xff0000
-  val CANCELED: Colour = 0xffff00
-  val RUNNING: Colour = 0xe89d13
-  val SUCCESS: Colour = 0x30ff49
+  val FAILED: Colour   = 0xFF0000
+  val CANCELED: Colour = 0xFFFF00
+  val RUNNING: Colour  = 0xE89D13
+  val SUCCESS: Colour  = 0x30FF49
 }
