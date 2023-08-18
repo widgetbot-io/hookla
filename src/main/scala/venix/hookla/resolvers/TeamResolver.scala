ce51f9abf99432fd10d0f378a8c8d9fe6221c95a
Daave <lewis@widgetbot.io>
2023-08-18 17:21:57 +0100

    feat: Create mutations for create, update, delete and members on Team

--- src/main/scala/venix/hookla/resolvers/TeamResolver.scala
+++ src/main/scala/venix/hookla/resolvers/TeamResolver.scala
@@ -1,16 +1,26 @@
 package venix.hookla.resolvers
 
+import venix.hookla.RequestError.Forbidden
 import venix.hookla.{Result, Task}
 import venix.hookla.entities.{Hook, Team, User}
 import venix.hookla.http.Auth
 import venix.hookla.services.db.{IHookService, ITeamService, IUserService}
 import venix.hookla.services.http.IDiscordUserService
-import venix.hookla.types.TeamId
-import zio.ZLayer
+import venix.hookla.types.{TeamId, UserId}
+import zio.{ZIO, ZLayer}
 
 trait ITeamResolver {
+  // Queries
   def getForMe: Task[List[Team]]
 
+  // Mutations
+  def create(name: String): Task[Team]
+  def update(id: TeamId, name: String): Task[Team]
+  def delete(id: TeamId): Task[Unit]
+  def addUser(id: TeamId, userId: UserId): Task[Unit]
+  def removeUser(id: TeamId, userId: UserId): Task[Unit]
+
+  // Resolvers
   def resolveMembers(id: TeamId): Result[List[User]]
   def resolveHooks(id: TeamId): Result[List[Hook]]
 }
@@ -22,6 +32,70 @@ class TeamResolver(
     private val hookService: IHookService
 ) extends ITeamResolver {
   def getForMe: Task[List[Team]] = Auth.currentUser.flatMap(user => teamService.getTeamsForUser(user.id).map(_.map(_.toEntity)))
+
+  def create(name: String): Task[Team] = Auth.currentUser.flatMap(user => teamService.create(name, "No description specified.", user.id).map(_.toEntity))
+
+  def update(id: TeamId, name: String): Task[Team] = Auth.currentUser.flatMap { user =>
+    teamService.getById(id).flatMap {
+      case Some(team) =>
+        teamService.getMembers(team.id).flatMap { members =>
+          if (members.exists { case (u, isAdmin) => u.id == user.id && isAdmin }) {
+            teamService.update(id, Some(name), None).as(team.toEntity.copy(name = name))
+          } else {
+            ZIO.fail(Forbidden("You do not have the required permissions to update this team."))
+          }
+        }
+      case _ => ZIO.fail(Forbidden("You do not have the required permissions to update this team."))
+    }
+  }
+
+  def delete(id: TeamId): Task[Unit] = Auth.currentUser.flatMap { user =>
+    teamService.getById(id).flatMap {
+      case Some(team) =>
+        teamService.getMembers(team.id).flatMap { members =>
+          if (members.exists { case (u, isAdmin) => u.id == user.id && isAdmin }) {
+            teamService.delete(id)
+          } else {
+            ZIO.fail(Forbidden("You do not have the required permissions to delete this team."))
+          }
+        }
+      case _ => ZIO.fail(Forbidden("You do not have the required permissions to delete this team."))
+    }
+  }
+
+  def addUser(id: TeamId, userId: UserId): Task[Unit] = Auth.currentUser.flatMap { user =>
+    teamService.getById(id).flatMap {
+      case Some(team) =>
+        teamService.getMembers(team.id).flatMap { members =>
+          if (members.exists { case (u, isAdmin) => u.id == user.id && isAdmin }) {
+            userService.getById(userId).flatMap {
+              case Some(user) => teamService.addUser(team.id, user.id)
+              case _          => ZIO.fail(Forbidden("The user you are trying to add does not exist."))
+            }
+          } else {
+            ZIO.fail(Forbidden("You do not have the required permissions to add a user to this team."))
+          }
+        }
+      case _ => ZIO.fail(Forbidden("You do not have the required permissions to add a user to this team."))
+    }
+  }
+
+  def removeUser(id: TeamId, userId: UserId): Task[Unit] = Auth.currentUser.flatMap { user =>
+    teamService.getById(id).flatMap {
+      case Some(team) =>
+        teamService.getMembers(team.id).flatMap { members =>
+          if (members.exists { case (u, isAdmin) => u.id == user.id && isAdmin }) {
+            userService.getById(userId).flatMap {
+              case Some(user) => teamService.removeUser(team.id, user.id)
+              case _          => ZIO.fail(Forbidden("The user you are trying to remove does not exist."))
+            }
+          } else {
+            ZIO.fail(Forbidden("You do not have the required permissions to remove a user from this team."))
+          }
+        }
+      case _ => ZIO.fail(Forbidden("You do not have the required permissions to remove a user from this team."))
+    }
+  }
 
   def resolveMembers(id: TeamId): Result[List[User]] = teamService.getMembers(id).map(_.map { case (user, isAdmin) => user.toEntity(isAdmin) })
   def resolveHooks(id: TeamId): Result[List[Hook]]   = hookService.getByTeam(id).map(_.map(_.toEntity))
--- src/main/scala/venix/hookla/services/db/TeamService.scala
+++ src/main/scala/venix/hookla/services/db/TeamService.scala
@@ -10,6 +10,12 @@ trait ITeamService extends BaseDBService {
   def getById(id: TeamId): Result[Option[Team]]
   def getTeamsForUser(id: UserId): Result[List[Team]]
   def getMembers(id: TeamId): Result[List[(User, Boolean)]]
+
+  def create(name: String, description: String, userId: UserId): Result[Team]
+  def update(id: TeamId, name: Option[String], description: Option[String]): Result[Unit]
+  def addUser(teamId: TeamId, userId: UserId, admin: Boolean = false): Result[Unit]
+  def removeUser(teamId: TeamId, userId: UserId): Result[Unit]
+  def delete(id: TeamId): Result[Unit]
 }
 
 class TeamService(
@@ -42,6 +48,66 @@ class TeamService(
         .filter { case (tu, _) => tu.teamId == lift(id) }
     }
       .mapBoth(DatabaseError, _.map { case (tu, u) => (u, tu.admin) }.toList)
+      .provide(ZLayer.succeed(ctx))
+
+  def create(name: String, description: String, userId: UserId) =
+    run {
+      teams
+        .insert(_.name -> lift(name))
+        .returning(x => x)
+    }
+      .mapError(DatabaseError)
+      .tap { t =>
+        run {
+          teamUsers
+            .insert(
+              _.teamId -> lift(t.id),
+              _.userId -> lift(userId),
+              _.admin  -> lift(true)
+            )
+        }
+          .mapError(DatabaseError)
+      }
+      .provide(ZLayer.succeed(ctx))
+
+  def update(id: TeamId, name: Option[String], description: Option[String]) =
+    run {
+      teams.dynamic
+        .filter(_.id == lift(id))
+        .update(setOpt(_.name, name))
+    }
+      .mapBoth(DatabaseError, _ => ())
+      .provide(ZLayer.succeed(ctx))
+
+  def addUser(teamId: TeamId, userId: UserId, admin: Boolean = false) =
+    run {
+      teamUsers
+        .insert(
+          _.teamId -> lift(teamId),
+          _.userId -> lift(userId),
+          _.admin  -> lift(admin)
+        )
+    }
+      .mapBoth(DatabaseError, _ => ())
+      .provide(ZLayer.succeed(ctx))
+
+  def removeUser(teamId: TeamId, userId: UserId) =
+    run {
+      teamUsers
+        .filter(_.teamId == lift(teamId))
+        .filter(_.userId == lift(userId))
+        .delete
+    }
+      .mapBoth(DatabaseError, _ => ())
+      .provide(ZLayer.succeed(ctx))
+
+  def delete(id: TeamId) =
+    run {
+      teams
+        .filter(_.id == lift(id))
+        .delete
+    }
+      .mapBoth(DatabaseError, _ => ())
       .provide(ZLayer.succeed(ctx))
 }
 
