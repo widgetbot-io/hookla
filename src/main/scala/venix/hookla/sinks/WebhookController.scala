60826d534cf4c6fa5ede1a0d34e195bd3bc2f9dd
Daave <lewis@widgetbot.io>
2023-08-18 20:39:46 +0100

    refactor: Change WebhookController too

--- src/main/scala/venix/hookla/sinks/WebhookController.scala
+++ src/main/scala/venix/hookla/sinks/WebhookController.scala
@@ -4,7 +4,7 @@ import venix.hookla.Result
 import zio.ZLayer
 import zio.http.Request
 
-trait IWebhookController {
+trait WebhookController {
   def handleWebhook(request: Request): Result[Unit]
 }
 
@@ -12,13 +12,13 @@ trait IWebhookController {
   * This class contains the handling of the webhooks that are INCOMING from sources
   * such as GitHub, GitLab, BitBucket, Sonarr, Radarr, etc..
   */
-class WebhookController extends IWebhookController {
+private class WebhookControllerImpl extends WebhookController {
   def handleWebhook(request: Request): Result[Unit] = ???
 }
 
 object WebhookController {
   private type In = Any
-  private def create() = new WebhookController()
+  private def create() = new WebhookControllerImpl()
 
-  val live: zio.ZLayer[In, Throwable, IWebhookController] = ZLayer.fromFunction(create _)
+  val live: zio.ZLayer[In, Throwable, WebhookController] = ZLayer.fromFunction(create _)
 }
