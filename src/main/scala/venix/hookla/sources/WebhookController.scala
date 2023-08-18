926fb9789e5f6ab95e57fd76a397929a7afdc516
Daave <lewis@widgetbot.io>
2023-08-18 22:34:57 +0100

    feat: Start webhook handler endpoint app

---
+++ src/main/scala/venix/hookla/sources/WebhookController.scala
@@ -0,0 +1,63 @@
+package venix.hookla.sources
+
+import io.circe.Json
+import sttp.tapir.{Endpoint, PublicEndpoint}
+import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
+import venix.hookla.Env
+import venix.hookla.RequestError.BadRequest
+import venix.hookla.services.db.HookService
+import venix.hookla.types.HookId
+import zio.http.{Body, HttpApp, Request, Response, Status}
+import zio.{&, RIO, ZIO, ZLayer}
+import sttp.tapir.ztapir._
+
+import java.util.UUID
+
+trait WebhookController {
+  def makeHttpService[R](implicit serverOptions: ZioHttpServerOptions[R] = ZioHttpServerOptions.default[R]): HttpApp[R & Env, Throwable]
+}
+
+/**
+  * This class contains the handling of the webhooks that are INCOMING from sources
+  * such as GitHub, GitLab, BitBucket, Sonarr, Radarr, etc..
+  */
+private class WebhookControllerImpl(
+    private val hookService: HookService
+) extends WebhookController {
+  // URI: /api/v1/handle/:hookId
+  // Method: POST
+  private def handleWebhook(request: Request): ZIO[Env, String, String] = (for {
+    _ <- ZIO.unit // just to start the for comprehension
+
+    maybeHookId = request.url.path.dropTrailingSlash.last
+    _ <- ZIO.fail(BadRequest("You need to pass a webhook ID.")) when maybeHookId.isEmpty
+
+    // TODO: This is a bit ugly, but it works for now.
+    hookId <- ZIO.attempt(UUID.fromString(maybeHookId.get)).map(HookId(_))
+
+    hook <- hookService.get(hookId)
+    _    <- ZIO.fail(BadRequest("Invalid webhook ID.")) when hook.isEmpty
+    // TODO: Update last used timestamp
+
+    handler <- SourceHandler.getHandlerById(hook.get.sourceId)
+    eventHandler = handler.determineEvent(request)
+
+    _ <- eventHandler.handle(request, hook.get)
+  } yield Json.obj("message" -> Json.fromString("")).spaces2).orElseFail("temp") // TODO: Fix out how to have better errors here.
+
+  private def webhookEndpoint = endpoint
+    .in(extractFromRequest[Request](x => x.underlying.asInstanceOf[Request]))
+    .errorOut(stringBody)
+    .out(stringJsonBody)
+
+  def makeHttpService[R](implicit serverOptions: ZioHttpServerOptions[R]): HttpApp[R & Env, Throwable] =
+    ZioHttpInterpreter(serverOptions)
+      .toHttp(webhookEndpoint.zServerLogic(handleWebhook))
+}
+
+object WebhookController {
+  private type In = HookService
+  private def create(hookService: HookService) = new WebhookControllerImpl(hookService)
+
+  val live: zio.ZLayer[In, Throwable, WebhookController] = ZLayer.fromFunction(create _)
+}
