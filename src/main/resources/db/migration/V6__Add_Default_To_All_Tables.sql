9c1dfcfb49f04b9e7fface2e25251ec425966bee
Lewis Tierney <lewis@widgetbot.io>
2026-04-29 23:23:26 +0100

    fix: Enable uuid-ossp extension in V6 migration

--- src/main/resources/db/migration/V6__Add_Default_To_All_Tables.sql
+++ src/main/resources/db/migration/V6__Add_Default_To_All_Tables.sql
@@ -1,3 +1,4 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 ALTER TABLE users ALTER COLUMN id SET DEFAULT uuid_generate_v4();
 ALTER TABLE discord_webhooks ALTER COLUMN id SET DEFAULT uuid_generate_v4();
 ALTER TABLE provider_settings ALTER COLUMN id SET DEFAULT uuid_generate_v4();
